8684279c10b9be0214c59677dd8186877c8c807f
xandy <>
2025-02-21 13:52:45 -0400

    /init: start project

--- src/main/java/com/zander404/springandjavaerudio/controller/GreetingsController.java
+++ src/main/java/com/zander404/springandjavaerudio/controller/GreetingsController.java
@@ -1,4 +1,28 @@
 package com.zander404.springandjavaerudio.controller;
 
+import com.zander404.springandjavaerudio.entities.Greetings;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@RequestMapping("/greetings")
 public class GreetingsController {
+
+    private final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+
+    @GetMapping("/")
+    public Greetings greetings(
+            @RequestParam(value = "name", defaultValue = "World") String name
+    ) {
+        return new Greetings(counter.incrementAndGet(), String.format(template, name));
+
+    }
+
+
 }
--- src/main/java/com/zander404/springandjavaerudio/entities/Greetings.java
+++ src/main/java/com/zander404/springandjavaerudio/entities/Greetings.java
@@ -1,4 +1,15 @@
 package com.zander404.springandjavaerudio.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor @NoArgsConstructor
 public class Greetings {
+
+    private Long id;
+    private String message;
+
+
 }
