1ec7d40ea4703b74abd33ad1220e59c8ac930421
xandy <>
2025-02-25 15:19:44 -0400

    /feat: Add Flyway to make migrations

---
+++ src/main/resources/db/migration/V2__Insert_Person.sql
@@ -0,0 +1,3 @@
+insert into aulajpa.person (id, address, first_name, gender, last_name)
+values  (1, 'San Francisco, CA', 'Leadro', 'M', 'Marcos'),
+        (2, 'San Francisco, CA', 'Leadro', 'M', 'Marcos');
